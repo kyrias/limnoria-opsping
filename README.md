68783c37bb1807213e9eea999b3757deb0dc8966
Johannes Löthberg <johannes@kyriasis.com>
2017-11-23 23:01:35 +0100

    Initial commit
    Signed-off-by: Johannes Löthberg <johannes@kyriasis.com>

---
+++ README.md
@@ -0,0 +1,1 @@
+Plugin for ops pinging message per channel
---
+++ plugin.py
@@ -0,0 +1,72 @@
+###
+# Copyright (c) 2017, Johannes Löthberg
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#   * Redistributions of source code must retain the above copyright notice,
+#     this list of conditions, and the following disclaimer.
+#   * Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions, and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#   * Neither the name of the author of this software nor the name of
+#     contributors to this software may be used to endorse or promote products
+#     derived from this software without specific prior written consent.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+###
+
+import supybot.conf as conf
+import supybot.utils as utils
+from supybot.commands import *
+import supybot.plugins as plugins
+import supybot.ircutils as ircutils
+import supybot.callbacks as callbacks
+try:
+    from supybot.i18n import PluginInternationalization
+    _ = PluginInternationalization('Ops')
+except ImportError:
+    # Placeholder that allows to run the plugin on a bot
+    # without the i18n module
+    _ = lambda x: x
+
+
+class Ops(callbacks.Plugin):
+    """Plugin for ops pinging message per channel"""
+
+    def ops(self, irc, msg, args):
+        """takes no arguments
+        Pings all ops in the current channel.
+        """
+        channel = msg.args[0]
+        opslist = self.registryValue('opslist', channel)
+        opslist = ', '.join(opslist)
+        if not opslist:
+            irc.error('no list of ops to ping configured for this channel.')
+            return
+
+        message = self.registryValue('message', channel)
+        if message:
+            message = '{} - {}'.format(opslist, message)
+        else:
+            message = opslist
+        irc.reply(message, prefixNick=False)
+    ops = wrap(ops)
+
+
+Class = Ops
+
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
